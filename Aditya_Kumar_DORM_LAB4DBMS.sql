81cd42f621c0febd39b76b09fcbaa84ad797ebfe
Aditya Kumar <110289734+AdityaKumar1999@users.noreply.github.com>
2022-11-30 23:50:37 +0530

    Update Aditya_Kumar_DORM_LAB4DBMS.sql

--- Aditya_Kumar_DORM_LAB4DBMS.sql
+++ Aditya_Kumar_DORM_LAB4DBMS.sql
@@ -62,9 +62,9 @@ drop database if exists E_commerce;
     RAT_RATSTARS INT NOT NULL
 );
     
-    insert 	into 	supplier	values	(	1,"	Rajesh Retails","Delhi"	,	1234567890	);
+insert 	into 	supplier	values	(	1       ,       "	Rajesh Retails  "       ,       "       Delhi   "	,	1234567890	);
 insert 	into 	supplier	values	(	2	,	"	Appario Ltd.	"	,	"	Mumbai	"	,	2589631470	);
-insert 	into 	supplier	values	(	3	,	"	Knome products	"	,	"	Banglore	"	,	9785462315	);
+insert 	into 	supplier	values	(	3	,	"	Knome products	"	,	"	Banglore"	,	9785462315	);
 insert 	into 	supplier	values	(	4	,	"	Bansal Retails	"	,	"	Kochi	"	,	8975463285	);
 insert 	into 	supplier	values	(	5	,	"	Mittal Ltd.	"	,	"	Lucknow	"	,	7898456532	);
 insert 	into 	customer	values	(	1	,	"	AAKASH	"	,	9999999999	,	"	DELHI	"	,'M');
